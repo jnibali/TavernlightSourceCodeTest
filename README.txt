86005f18b854e47d04edf0d2c768f0193345bde5
Joe Nibali <joenibali27@gmail.com>
2024-04-21 10:48:53 -0700

    Question 5 commit

---
+++ README.txt
@@ -0,0 +1,4 @@
+Attached is the source code and data files for both "The Forgotten Server" and "OTC Client". 
+Please replace your source and data folders with the ones in this repo to run the code on your end. 
+
+Please open the text and video files labeled for each question for a explanation of the feature implemented 
--- TFS/data/spells/scripts/support/light.lua
+++ TFS/data/spells/scripts/support/light.lua
@@ -2,12 +2,21 @@ local combat = Combat()
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
 combat:setParameter(COMBAT_PARAM_AGGRESSIVE, false)
 
-local condition = Condition(CONDITION_LIGHT)
-condition:setParameter(CONDITION_PARAM_LIGHT_LEVEL, 6)
-condition:setParameter(CONDITION_PARAM_LIGHT_COLOR, 215)
-condition:setParameter(CONDITION_PARAM_TICKS, (6 * 60 + 10) * 1000)
-combat:addCondition(condition)
+
 
 function onCastSpell(creature, variant)
-	return combat:execute(creature, variant)
+	for i = 10,1,-1 --for loop for amount of effects to spawn
+	do
+		
+		local position = creature:getPosition() --get the position of the player casting the spell
+		position.x = position.x + math.random(-2,2) --randomize posistion of effect around player x and y
+		position.y = position.y +math.random(-2,2)
+		-- Better density effect if effect is casted again in same loop iteration
+		doSendMagicEffect(position, CONST_ME_ICETORNADO) 
+		local position = creature:getPosition() 
+		position.x = position.x + math.random(-2,2) 
+		position.y = position.y +math.random(-2,2)
+		doSendMagicEffect(position, CONST_ME_ICETORNADO) 
+	end
+	--return creature:conjureItem(0, 2543, 5, CONST_ME_MAGIC_BLUE)  --debug statement to verify spell is being called
 end
